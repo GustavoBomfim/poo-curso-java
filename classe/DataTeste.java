d5ef99879bc4d11001c80f585cab7119588ba139
GustavoBomfim <gustavobomfim@alunos.utfpr.edu.br>
2022-10-24 16:58:21 -0300

    Aula 102 - Desafio Construtor

--- classe/DataTeste.java
+++ classe/DataTeste.java
@@ -3,14 +3,9 @@ package classe;
 public class DataTeste {
     public static void main(String[] args) {
         Data d1 = new Data();
-        d1.dia = 01;
-        d1.mes = 03;
-        d1.ano = 2001;
+        d1.ano = 2022;
 
-        var d2 = new Data();
-        d2.dia = 15;
-        d2.mes = 03;
-        d2.ano = 2005;
+        var d2 = new Data(01, 03, 2001);
 
         String dataFormatada1 = d1.obterDataFormatada();
 
