5b44bffc25ab5b421be18e097a15eeec484873bf
GustavoBomfim <gustavobomfim@alunos.utfpr.edu.br>
2022-10-25 14:42:48 -0300

    Aula 110 - Desafio Primeiro Trauma

---
+++ classe/PrimeiroTrauma.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+    int a = 3;
+    static int b =4;
+
+    public static void main(String[] args) {
+
+        PrimeiroTrauma primeiroTrauma = new PrimeiroTrauma();
+        System.out.println(primeiroTrauma.a);
+        
+        System.out.println(b);
+    }
+}
