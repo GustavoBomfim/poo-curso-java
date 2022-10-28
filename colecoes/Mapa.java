3cbfc4894c01ebdc38632dda964bd60eb48d5fa4
GustavoBomfim <gustavobomfim@alunos.utfpr.edu.br>
2022-10-28 10:35:48 -0300

    Aula 134 - Map

---
+++ colecoes/Mapa.java
@@ -0,0 +1,45 @@
+package colecoes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapa {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> usuarios = new HashMap<>();
+
+        usuarios.put(1, "Roberto");
+        usuarios.put(2, "Ricardo");
+        usuarios.put(3, "Rafaela");
+        usuarios.put(4, "Rebeca");
+
+        System.out.println(usuarios.size());
+        System.out.println(usuarios.isEmpty());
+
+        System.out.println(usuarios.keySet());
+        System.out.println(usuarios.values());
+        System.out.println(usuarios.entrySet());
+
+        System.out.println(usuarios.containsKey(2));
+        System.out.println(usuarios.containsValue("Rebeca"));
+
+        System.out.println(usuarios.get(2));
+        System.out.println(usuarios.remove(1));
+
+        //Retornará false pois não há um valor com nome "Gui"
+        System.out.println(usuarios.remove(1, "Gui"));
+
+        for (int chave: usuarios.keySet()){
+            System.out.println(chave);
+        }
+        for (String valor: usuarios.values()){
+            System.out.println(valor);
+        }
+        for (Map.Entry<Integer, String> registro: usuarios.entrySet()){
+            System.out.print(registro.getKey() + " ==> ");
+            System.out.println(registro.getValue());
+
+        }
+    }
+}
