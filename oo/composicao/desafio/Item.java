fa3fc6ba088dadc46138137e11c009e7229cd68c
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-11-02 11:48:31 -0300

    Aula 144 - Desafio Composição

---
+++ oo/composicao/desafio/Item.java
@@ -0,0 +1,20 @@
+package oo.composicao.desafio;
+
+import oo.composicao.Motor;
+
+public class Item {
+
+    final Produto produto;// = new Produto();
+    final int quantidade;
+
+    Item(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+
+
+    }
+
+
+
+
+}
