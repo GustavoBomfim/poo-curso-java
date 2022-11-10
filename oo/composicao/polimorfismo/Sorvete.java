2b7c10b4559632ac5ffcf677b861597e0f57e629
GustavoBomfim <gustavobomfim@alunos.utfpr.edu.br>
2022-11-10 14:57:47 -0300

    Aula 164 - Polimorfismo

---
+++ oo/composicao/polimorfismo/Sorvete.java
@@ -0,0 +1,19 @@
+package oo.composicao.polimorfismo;
+
+public class Sorvete {
+
+    private double peso;
+
+    public Sorvete(double peso){
+        setPeso(peso);
+    }
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if(peso >= 0){
+            this.peso = peso;
+        }
+    }
+}
